ddf732ea8f6d69b6267761b2dc5f5930eacba858
Suman Kumar Ghosh <78735192+sumankumar86@users.noreply.github.com>
2023-01-14 14:52:20 +0530

    Add files via upload

---
+++ Aggregration/Aggregration/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aggregration
+{
+    class Details
+    {
+        public string addressline;
+        public int pin;
+        public Details(string addressline,int pin)
+        {
+            this.addressline = addressline;
+            this.pin = pin;
+        }
+    }
+    class Employee
+    {
+        private string name;
+        private Details address;
+
+        public Employee(string name,Details address)
+        {
+            this.name = name;
+            this.address = address;
+        }
+        public void display()
+        {
+            Console.WriteLine($"Name is {name} and address is {address.addressline} and pin is {address.pin}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Details det = new Details("Kolkata", 713121);
+            Employee obj = new Employee("Suman", det);
+            obj.display();
+        }
+    }
+}
---
+++ UserdefinedException/UserdefinedException/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UserdefinedException
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Enter age:");
+            int age=int.Parse(Console.ReadLine());
+            try
+            {
+                if (age > 18)
+                {
+                    Console.WriteLine("You are valid");
+                }
+                else
+                {
+                    Thread.Sleep(1000);
+                    throw new Exception("You are not valid");
+                }
+            }
+         
+            catch(Exception e) 
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Thread.Sleep(3000);
+            Console.WriteLine("Rest of the code");
+         
+        }
+    }
+}
